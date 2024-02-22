02bb3a8810d204de634cf64aba6fd5aafe347bb0
Saidalo1 <saidakhmedovsaidalo@gmail.com>
2024-02-22 18:14:19 +0500

    Fix http to https

--- root/settings/__init__.py
+++ root/settings/__init__.py
@@ -1,6 +1,7 @@
 from root.settings.ckeditor_configs import customColorPalette, CKEDITOR_5_CONFIGS
 from root.settings.custom_settings import CSRF_TRUSTED_ORIGINS, CELERY_RESULT_EXTENDED, CELERY_RESULT_BACKEND, \
-    qualities, INTERNAL_IPS, languages_to_create, languages_to_create_keys, CORS_ALLOWED_ORIGINS
+    qualities, INTERNAL_IPS, languages_to_create, languages_to_create_keys, CORS_ALLOWED_ORIGINS, \
+    SECURE_PROXY_SSL_HEADER, USE_X_FORWARDED_HOST
 from root.settings.databases import DATABASES
 from root.settings.default_settings import *
 from root.settings.i18n import LANGUAGE_CODE, TIME_ZONE, USE_I18N, USE_TZ, PARLER_LANGUAGES, LOCALE_PATHS

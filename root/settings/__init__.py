6a409f219655a794bcaeeb2f664cf14c5373e92e
Saidalo1 <saidakhmedovsaidalo@gmail.com>
2024-02-22 17:36:13 +0500

    Add import settings from package

--- root/settings/__init__.py
+++ root/settings/__init__.py
@@ -1,6 +1,6 @@
 from root.settings.ckeditor_configs import customColorPalette, CKEDITOR_5_CONFIGS
 from root.settings.custom_settings import CSRF_TRUSTED_ORIGINS, CELERY_RESULT_EXTENDED, CELERY_RESULT_BACKEND, \
-    qualities, INTERNAL_IPS, languages_to_create, languages_to_create_keys
+    qualities, INTERNAL_IPS, languages_to_create, languages_to_create_keys, CORS_ALLOWED_ORIGINS
 from root.settings.databases import DATABASES
 from root.settings.default_settings import *
 from root.settings.i18n import LANGUAGE_CODE, TIME_ZONE, USE_I18N, USE_TZ, PARLER_LANGUAGES, LOCALE_PATHS
